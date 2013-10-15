6cb90cc6d0776010fc9bf60ab61f5f4747a5fd8c
Brolita <Byrnec3@rpi.edu>
2013-10-15 17:51:00 -0400

    Update hw2.js

--- hw2.js
+++ hw2.js
@@ -1,5 +1,5 @@
-//Javascript File for hw2
  $.fn.hexed = function(settings) {
+	$("#display").width(200).height(200);
 	var difficulty = -1;
 	var turns = -1;
 	if(settings != null) {
@@ -14,4 +14,12 @@
 		turns = 10;
 	if(difficulty == -1)
 		difficulty = 5;
+	
+	for(var i = 0; i < turns; i++) {
+		var r = Math.floor(255*Math.random());
+		var g = Math.floor(255*Math.random());
+		var b = Math.floor(255*Math.random());
+		var randomColor = "rgb(" + r.toString() + ", " + g.toString() + ", " + b.toString() + ")"
+		$("#display").css("background-color", randomColor);
+	}
 }
