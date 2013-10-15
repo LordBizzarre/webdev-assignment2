ed577ebf8f36d4d4bf74025b6aaaef73ead6a984
Brolita <Byrnec3@rpi.edu>
2013-10-15 17:01:45 -0400

    Update hw2.js
    Basics for the 'hexed' function, takes the arguments, and sets the variables 'difficulty' and 'turns' accordingly.

--- hw2.js
+++ hw2.js
@@ -1,1 +1,17 @@
 //Javascript File for hw2
+ $.fn.hexed = function(settings) {
+	var difficulty = -1;
+	var turns = -1;
+	if(settings != null) {
+		for item in settings {
+			if(item.first = "difficulty")
+				difficulty = item.second;
+			else if(item.first = "turns")
+				turns = item.second;
+		}
+	}
+	if(turns == -1)
+		turns = 10;
+	if(difficulty == -1)
+		difficulty = 5;
+}
