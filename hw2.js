6672b78dfedb01f5987c1f5d0180d9188cc6173c
Brolita <Byrnec3@rpi.edu>
2013-10-16 02:06:10 -0400

    Update hw2.js

--- hw2.js
+++ hw2.js
@@ -1,9 +1,9 @@
-$.fn.hexed = function(settings) {
-	$("#display").width(200).height(200);
-	var difficulty = -1;
-	var turns = -1;
+$.fn.hexed = function(settings) { //makes 'hexed' a jquery function
+	$("#display").width(200).height(200); //sets size of display
+	var difficulty = -1; //param check
+	var turns = -1; 
 	if(settings != null) {
-		for item in settings {
+		for (item in settings) {
 			if(item.first = "difficulty")
 				difficulty = item.second;
 			else if(item.first = "turns")
@@ -15,14 +15,49 @@ $.fn.hexed = function(settings) {
 	if(difficulty == -1)
 		difficulty = 5;
 	
-	for(var i = 0; i < turns; i++) {
-		var r = Math.floor(255*Math.random());
-		var g = Math.floor(255*Math.random());
-		var b = Math.floor(255*Math.random());
+	var function2;
+	var start;
+	var r;
+	var g;
+	var b;
+	
+	var function1 = function () { //listen for the first click
+		start =  new Date().getTime(); // to get the time simply recall the RSA
+									   // and subtract start from it
+		this.innerHTML = "Got it!"; //change text of button
+		r = Math.floor(255*Math.random()); //generate and show color
+		g = Math.floor(255*Math.random());
+		b = Math.floor(255*Math.random());
 		var randomColor = "rgb(" + r.toString() + ", " + g.toString() + ", " + b.toString() + ")"
 		$("#display").css("background-color", randomColor);
-	}
-}
+	};
+	
+	function2 = function() { //listen for a second click
+		this.innerHTML = "Start!";
+		var redGuess = document.forms["red"]["val"].value; //get their color guess
+		var greenGuess = document.forms["green"]["val"].value;
+		var blueGuess = document.forms["blue"]["val"].value;
+		if(redGuess == null || greenGuess == null || blueGuess == null ||
+			  redGuess == "" || greenGuess == "" || greenGuess == "") {
+			alert("You need to pick a value for all 3 sliders!");
+			this.click(function2);
+		} else {
+			turns--; //decrement turns 
+			//using Plotka's formula
+			var accuracy = (Math.abs(r - parseInt(redGuess)) + Math.abs(g - parseInt(greenGuess)) 
+					  + Math.abs(b - parseInt(blueGuess)))/765
+			var milliseconds_taken =  new Date().getTime() - start;
+			var score = Math.floor(((15 - difficulty - accuracy*100)/(15-difficulty))
+					  * (15000-milliseconds_taken));
+			if(score < 0)
+				score = 0;
+			
+			$("#score_board").text( score.toString() );
+		}
+	};
+	this.toggle(function1, function2); //sets the first listener to function 1
+	return this;
+};
 
 function refreshVal() {
     var red = $("#red_slider").slider("value")
