15549366f8cdb2ae9db719088eeb651b872f79dc
dorisxian <Doris@dharma-12.dynamic2.rpi.edu>
2013-10-17 15:13:43 -0400

    Update new set of files. (working without bugs)
    update some functionality but don't know how to work as a plugin.

---
+++ jquery.hexed.js
@@ -0,0 +1,15 @@
+(function($) {
+
+    $.fn.hexed = function(options) {
+        // Establish default settings
+        var 
+        defaults = {
+            difficulty: 5,
+            turns: 10
+        },
+        settings = $.extend({}, defaults, options);
+        
+
+
+    }
+}(jQuery));
--- team1-a2.js
+++ team1-a2.js
@@ -1,60 +1,63 @@
 //$.fn.hexed = function() {
 $(function(){
 
-  var turns = 10;
-
   var randomCol = '#'+Math.floor(Math.random()*16777215).toString(16);
   $("#rand_swatch").css("background-color", randomCol);
 
   $( "#red, #green, #blue" ).slider({
     orientation: "horizontal",
     range: "min",
+    min: 0,
     max: 255,
-    value: 127,
     slide: refreshSwatch,
     change: refreshSwatch
   });
   $( "#red" ).slider( "value", 0 );
   $( "#green" ).slider( "value", 0 );
   $( "#blue" ).slider( "value", 0 );
-});
 
-function hexFromRGB(r, g, b) {
-  var hex = [
-    r.toString( 16 ),
-    g.toString( 16 ),
-    b.toString( 16 )
-  ];
-  $.each( hex, function( nr, val ) {
-    if ( val.length === 1 ) {
-      hex[ nr ] = "0" + val;
-    }
+  $( "#input_red" ).change(function() {
+    $( "#red" ).slider( "value", $(this).val() );
   });
-  return hex.join( "" ).toUpperCase();
-}
+  $( "#input_green" ).change(function() {
+    $( "#green" ).slider( "value", $(this).val() );
+  });
+  $( "#input_blue" ).change(function() {
+    $( "#blue" ).slider( "value", $(this).val() );
+  });
+
+});
 
 function refreshSwatch() {
   var 
   red = $( "#red" ).slider( "value" ),
   green = $( "#green" ).slider( "value" ),
   blue = $( "#blue" ).slider( "value" ),
-  hex = hexFromRGB( red, green, blue );
-  $( "#swatch" ).css( "background-color", "#" + hex );
-  $("#rval").text("("+red+")");
-  $("#gval").text("("+green+")");
-  $("#bval").text("("+blue+")");
+  color = "rgb(" + red.toString() + ", " + green.toString() + ", " + blue.toString() + ")";
+  $( "#swatch" ).css( "background-color", color);
   $('#input_red').val(red);
   $('#input_green').val(green);
   $('#input_blue').val(blue);
 }
 
-function showdiv(id){
-  $(document.getElementById(id)).fadeToggle();
+function startGame () {
+  $("input[name=difficulty]").attr("disabled", "disabled");
+  $("input[name=turns]").attr("disabled", "disabled");
+  //alert($("input[name=difficulty]").val() + $("input[name=turns]").val());
+  $("#game").show("fast");
+}
+
+function showResult(){
+  $("#swatch").show("fast");
+  $("#result").show("fast");
+  var y = $(window).scrollTop();
+  $("#result").scrollTop(y + 800);  
   $( "#red, #green, #blue" ).slider({ disabled: true });
-  
+  $("#input_red, #input_green, #input_blue").attr("disabled", "disabled");
+  $("input[type=submit]").attr("disabled", "disabled");
 
 }
 
 function next(){
-  window.location.reload()
+  window.location.reload();
 }
